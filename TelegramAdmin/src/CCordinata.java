45162cffeb0de6ffd6fcc00194b9bf1c3779bdc2
matteoorig <matteoorig@gmail.com>
2022-03-07 12:02:57 +0100

    init

---
+++ TelegramAdmin/src/CCordinata.java
@@ -0,0 +1,21 @@
+public class CCordinata {
+    float lat;
+    float lon;
+
+    public CCordinata(){}
+
+    public void setLat(float l){
+        this.lat = l;
+    }
+    public void setLon(float l){
+        this.lon = l;
+    }
+
+    public float getLat() {
+        return lat;
+    }
+
+    public float getLon() {
+        return lon;
+    }
+}
---
+++ TelegramAdmin/src/main.java
@@ -0,0 +1,55 @@
+import org.xml.sax.SAXException;
+
+import javax.swing.*;
+import javax.xml.parsers.ParserConfigurationException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+public class main {
+    static telegramManager tm = new telegramManager("5220725554:AAE4s92QSGANF5cCjQ9CBB0FOacLYy8xS4Q");
+    static openMapManager map = new openMapManager();
+    public static void main(String[] args) {
+
+
+        JFrame frame = new JFrame("TELEGRAM ADMIN");
+        frame.setSize(600, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        frame.add(panel);
+        placeComponents(panel);
+
+        frame.setVisible(true);
+    }
+
+    private static void placeComponents(JPanel panel) {
+        panel.setLayout(null);
+
+        JButton getPosition = new JButton("Cordinata");
+        getPosition.setBounds(40, 40, 150, 25);
+        getPosition.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //get latitudine longitudine
+                CCordinata cord = new CCordinata();
+                try {
+                    cord = map.getPosition("Meda");
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                } catch (ParserConfigurationException ex) {
+                    ex.printStackTrace();
+                } catch (SAXException ex) {
+                    ex.printStackTrace();
+                }
+
+                System.out.println("Latitudine: "+ cord.getLat()+" Longitudine: "+ cord.getLon());
+
+            }
+        });
+        panel.add(getPosition);
+    }
+
+}
