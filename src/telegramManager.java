3f4f7b012924fc0b4270b6970af3e23fb886736c
matteoorig <matteoorig@gmail.com>
2022-03-08 12:52:00 +0100

    update

--- src/telegramManager.java
+++ src/telegramManager.java
@@ -1,3 +1,6 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.*;
 import java.net.URL;
 import java.util.ArrayList;
@@ -16,11 +19,7 @@ class telegramManager extends Thread{
         while(true){
 
 
-            try {
-                getUpdates();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+
             try {
                 sleep(1000);
             } catch (InterruptedException e) {
@@ -32,26 +31,39 @@ class telegramManager extends Thread{
     public void getUpdates() throws IOException {
         String tmpUrl =  formatUrl + "/getUpdates";
         URL url = new URL(tmpUrl);
-        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
-        String line;
-        while ((line = in.readLine()) != null) {
+        Scanner scan = new Scanner(url.openStream());
+        scan.useDelimiter("\u001a");
 
-            System.out.println(line);
+        String jsonString = scan.next();
+        JSONObject jsonObject = new JSONObject(jsonString);//prendo file completo formattato
+
+        JSONArray results = jsonObject.getJSONArray("result");
+        if(results.length() > 0){
+            for (int i = 0; i < results.length(); i++){
+                //int update_id = jsonObject.getJSONArray("result").getJSONObject(i).getInt("update_id");
+
+                long unicId = results.getJSONObject(i).getJSONObject("message").getJSONObject("chat").getInt("id");
+                String first_name = results.getJSONObject(i).getJSONObject("message").getJSONObject("chat").getString("first_name");
+                String last_name = results.getJSONObject(i).getJSONObject("message").getJSONObject("chat").getString("last_name");
+
+
+                if(results.getJSONObject(i).getJSONObject("message").getString("text").equals("/"))
+                try (PrintWriter writer = new PrintWriter(new File("data.csv"))) {
+
+                    StringBuilder sb = new StringBuilder();
+                    sb.append(unicId+"-"+first_name+"-"+last_name);
+                    sb.append('\n');
+
+                    writer.write(sb.toString());
+                    writer.close();
+
+                } catch (FileNotFoundException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
         }
 
 
-        try (PrintWriter writer = new PrintWriter(new File("test.csv"))) {
-
-            StringBuilder sb = new StringBuilder();
-            sb.append("id");
-            sb.append('\n');
-
-            writer.write(sb.toString());
-            writer.close();
-
-        } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
-        }
 
     }
 
